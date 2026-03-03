7370eb384b508d9183cfd7a21e1084bbb7a983b5
Esraa Shiref <israashiref@gmail.com>
2026-03-03 12:42:01 +0200

    edited printLab2 method by Esraa

--- .history/HelloJava_20260303124133.java
+++ .history/HelloJava_20260303124133.java
@@ -5,7 +5,7 @@ public class HelloJava {
         System.out.println("Hello World from main branch");
     }
     public static void printLab2() {
-    System.out.println("Lab 2 Work - esraa branch");
+    System.out.println("Lab 2 Work - esraa");
     }
 
     public static void main(String[] args) {
