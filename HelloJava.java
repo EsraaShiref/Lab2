4acd9636e8c9c206384d2bdeba7809c9a30b4ac3
Esraa Shiref <israashiref@gmail.com>
2026-03-03 12:13:30 +0200

    Added printLab2 method by Esraa

--- HelloJava.java
+++ HelloJava.java
@@ -4,8 +4,12 @@ public class HelloJava {
     public static void printHelloWorld() {
         System.out.println("Hello World");
     }
+    public static void printLab2() {
+    System.out.println("Lab 2 Work - Esraa");
+    }
 
     public static void main(String[] args) {
         printHelloWorld();
+        printLab2();
     }
 }
