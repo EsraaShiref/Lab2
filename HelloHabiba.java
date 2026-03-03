730ea6ac3ff046d7e361005be771cb4b1722b6e7
HabibaNezar <habibanezarr.com>
2026-03-03 12:13:36 +0200

    added new class hello habiba

---
+++ HelloHabiba.java
@@ -0,0 +1,10 @@
+public class HelloJava {
+
+    public static void printHelloHabiba() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        printHelloHabiba();
+    }
+}
