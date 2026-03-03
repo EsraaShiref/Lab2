1d526b7bd1fababca283cfc4fc9231b892202005
Esraa Shiref <israashiref@gmail.com>
2026-03-03 12:18:22 +0200

    Edited line in main

--- .history/HelloJava_20260303121646.java
+++ .history/HelloJava_20260303121646.java
@@ -2,14 +2,10 @@
 public class HelloJava {
 
     public static void printHelloWorld() {
-        System.out.println("Hello World from main branch");
-    }
-    public static void printLab2() {
-    System.out.println("Lab 2 Work - Esraa");
+        System.out.println("Hello World");
     }
 
     public static void main(String[] args) {
         printHelloWorld();
-        printLab2();
     }
 }
--- .history/HelloJava_20260303121758.java
+++ .history/HelloJava_20260303121758.java
@@ -4,12 +4,8 @@ public class HelloJava {
     public static void printHelloWorld() {
         System.out.println("Hello World from main branch");
     }
-    public static void printLab2() {
-    System.out.println("Lab 2 Work - Esraa");
-    }
 
     public static void main(String[] args) {
         printHelloWorld();
-        printLab2();
     }
 }
