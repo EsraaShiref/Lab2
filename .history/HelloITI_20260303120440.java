0aa251c7debccfbe74a648dd058335f3d8bf1cf3
Esraa Shiref <israashiref@gmail.com>
2026-03-03 12:25:17 +0200

    Added printLab2 method in feature-esraa

--- .history/HelloITI_20260303120440.java
+++ .history/HelloITI_20260303120440.java
@@ -9,13 +9,8 @@ public class HelloITI {
         System.out.println("Hello Git");
     }
 
-    public static void printLab2() {
-    System.out.println("Lab2 Feature By Esraa");
-}
-
     public static void main(String[] args) {
         printHelloITI();
         printHelloGit();
-        printLab2();
     }
 }
